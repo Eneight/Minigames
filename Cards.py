a4ecd552649dc5b0549b5b2c15141370c9847848
Nate Berol <n8berol@gmail.com>
2021-09-27 19:03:36 -0400

    Debug Cards.py

--- Cards.py
+++ Cards.py
@@ -1,20 +1,21 @@
 class Card:
     def __init__(self, kind: str, suite: str):
-        self.kind = kind.lower
-        self.suite = suite.lower()
-        if int(self.kind) in range(1,10):
-            self.val = int(self.kind)
-        elif self.val in ["jack", "j", "queen", "q", "king", "k"]:
-            self.val = 10
-        elif self.kind in ["ace", "a"]:
-            self.val = 1
-
-    def getValue(self):
-        return self.val  
+        self.kind = kind
+        self.suite = suite
+        try:
+            if int(self.kind) in range(1,10):
+                self.val = int(self.kind)
+        except:
+            if self.kind in ["Jack", "Queen", "King"]:
+                self.val = 10
+            elif self.kind == "Ace":
+                self.val = 1
+            else:
+                raise ValueError
     
     def __str__(self):
-        return f"{self.val} of {self.suite}"
+        return f"{self.kind} of {self.suite}"
 
     def __repr__(self):
-        return f"Card({self.val}, {self.suite}"
+        return f"Card(\"{self.kind}\", \"{self.suite}\")"
         
