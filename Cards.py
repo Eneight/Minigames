571682878601ee3e05d772eb827254b18dd33995
Nate Berol <n8berol@gmail.com>
2021-09-30 20:59:24 -0400

    Fix self.val

--- Cards.py
+++ Cards.py
@@ -2,16 +2,15 @@ class Card:
     def __init__(self, kind: str, suite: str):
         self.kind = kind
         self.suite = suite
-        try:
+    
+        if self.kind in ["Jack", "Queen", "King"]:
+            self.val = 10
+        elif self.kind == "Ace":
+            self.val = 1
+        else:
             if int(self.kind) in range(1,10):
                 self.val = int(self.kind)
-        except:
-            if self.kind in ["Jack", "Queen", "King"]:
-                self.val = 10
-            elif self.kind == "Ace":
-                self.val = 1
-            else:
-                raise ValueError
+    
     
     def __str__(self):
         return f"{self.kind} of {self.suite}"
