2e28791ef3ca83ebdfba61b677c9941de0b39549
Nate Berol <n8berol@gmail.com>
2021-10-07 20:18:44 -0400

    Add Utility Functions

--- Deck.py
+++ Deck.py
@@ -1,4 +1,3 @@
-import random
 from Cards import Card
 
 class Deck:
@@ -6,10 +5,23 @@ class Deck:
         self.cards = [Card(kind, suite)
                     for kind in ["Ace", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Jack", "Queen", "King"]
                     for suite in ["Spades", "Hearts", "Diamonds", "Clubs"]]
+        self.len = len(self.cards)
 
-    def shuffle(self):
-        self.cards = random.shuffle(self.cards)
-                    
+    def deal(self, amount = 1):
+        results = []
+        for index in range(amount):
+            results.append(self.cards.pop())
+        return results
+                  
+    def __len__(self):
+        return len(self.cards)
+    
+    def __setitem__(self, key, value):
+        self.cards[key] = value
+        
+    def __getitem__(self, key):
+        return self.cards[key]
+    
     def __str__(self):
         return str(self.cards)
 
--- Player.py
+++ Player.py
@@ -4,8 +4,17 @@ class Player:
         self.name = name
         self.cards = cards
 
+    def __len__(self):
+        return len(self.cards)
+    
+    def __setitem__(self, key, value):
+        self.cards[key] = value
+        
+    def __getitem__(self, key):
+        return self.cards[key]
+
     def __str__(self):
-        return f"Name: {self.name}\tCards: {str(self.cards)}"
+        return f"Name: {self.name}\nCards: {str(self.cards)}"
 
     def __repr__(self):
         return f"Player(name={self.name}, cards={str(self.cards)})"
