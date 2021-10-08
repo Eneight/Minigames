d5ed79a8aa5d1f2afddfbac68eb279b48d8cbd5f
Nate Berol <n8berol@gmail.com>
2021-10-08 18:35:37 -0400

    Add val function

--- Cards.py
+++ Cards.py
@@ -10,7 +10,8 @@ class Card:
         else:
             if int(self.kind) in range(1,10):
                 self.val = int(self.kind)
-    
+    def val(self):
+            return self.val
     
     def __str__(self):
         return f"{self.kind} of {self.suite}"
