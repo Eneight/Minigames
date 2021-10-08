5aeb15c57f40aaf8b07f308026d529dac33de9a0
Nate Berol <n8berol@gmail.com>
2021-10-08 18:29:52 -0400

    Remove references to Player.py

--- War.py
+++ War.py
@@ -1,6 +1,5 @@
 from Cards import Card
 from Deck import Deck
-from Player import Player
 import random
 
 def main():
@@ -12,9 +11,9 @@ def setup():
     deck = Deck()
     random.shuffle(deck)
     global player1 
-    player1 = Player(input("Enter Name For Player 1: "), deck.deal(26))
+    player1 = Deck(input("Enter Name For Player 1: "), deck.deal(26))
     global player2
-    player2 = Player(input("Enter Name for Player 2: "), deck.deal(26))
+    player2 = Deck(input("Enter Name for Player 2: "), deck.deal(26))
 
 def play():
     pass
