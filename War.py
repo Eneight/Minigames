bb489ca9902e56cc7c1a7ea5873ec9879b180c36
Nate Berol <n8berol@gmail.com>
2021-10-08 19:01:03 -0400

    Save progress

--- War.py
+++ War.py
@@ -5,6 +5,10 @@ import random
 def main():
     setup()
     while len(player1) < 52 and len(player2) < 52:
+        random.shuffle(player1.cards)
+        random.shuffle(player2.cards)
+        global round_cards 
+        round_cards = []
         play()
 
 def setup():
@@ -15,8 +19,23 @@ def setup():
     global player2
     player2 = Deck(input("Enter Name for Player 2: "), deck.deal(26))
 
+
+#Being Implemented
 def play():
-    pass
+    p1_card = player1.deal()
+    print(f"{player1.name} drew {p1_card}.")
+    p2_card = player2.deal()
+    print(f"{player2.name} drew {p2_card}.")
+    round_cards.append([p1_card, p2_card])
+    if p1_card.val > p2_card.val:
+        print(f"{player1.name} wins the round and recieves all cards.")
+        player1.cards.append([p1_card, p2_card])
+    elif p1_card.val < p2_card.val:
+        print(f"{player2.name} wins the round and recieves all cards.")
+        player2.cards.append([p1_card, p2_card])
+    else:
+        
+    
    
 if __name__ == '__main__':
     main()
