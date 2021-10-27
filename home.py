f836125de58b20673d359b1e5267ac567b4a3433
Nate Berol <n8berol@gmail.com>
2021-10-26 20:53:33 -0400

    Add home.py

---
+++ home.py
@@ -0,0 +1,14 @@
+import pygame
+import pygame_menu
+
+pygame.init()
+
+surface = pygame.display.set_mode((0,0,), pygame.FULLSCREEN, pygame.RESIZABLE)
+pygame.display.set_caption("Minigames Player")
+
+menu = pygame_menu.Menu('Welcome', 400, 300, theme=pygame_menu.themes.THEME_BLUE)
+
+menu.add.text_input('Name :', default='John Doe')
+menu.add.button('Quit', pygame_menu.events.EXIT)
+
+menu.mainloop(surface)
